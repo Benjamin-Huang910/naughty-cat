123dd26d69023b325533ec95b49d76e9e4d6bf93
Benjamin-Huang910 <benjaminhuang910@gmail.com>
2020-03-17 20:18:51 +0800

    5

---
+++ 程式概論/week03_ex2_107304105.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=8
+y=34
+z=9*(4/x+(9+x)/y)
+z
+z/=3
+z
+
---
+++ 程式概論/week03_ex3_107304015.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+x=2000
+y=2001
+z=2018
+print(x%4==0)
+print(not(y%100==0))
+print(z//400>=5)
+print((x%4==0)and(not(y%100==0)))
+print((x%4==0)or(z%400==5))
+
